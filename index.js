9f3e286075617253c54aa8365d4fce5927a0360f
anepaz <anelisespaz@gmail.com>
2022-05-26 17:17:24 -0300

    adds scraped info to frontend

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const PORT = 8000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 const url = 'https://www.ufpr.br/portalufpr/'
 
-axios(url)
+app.get('/', function (req, res) {
+  res.json('My simple webscraper')
+})
+
+app.get('/results', (req, res) => {
+
+  axios(url)
   .then(response => {
     const html = response.data
     const $ = cheerio.load(html)
@@ -21,7 +28,9 @@ axios(url)
         url
       })
     })
-    console.log(articles)
+    res.json(articles)
   }).catch(err => console.log(err))
+
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
---
+++ src/app.js
@@ -0,0 +1,11 @@
+const newsFeed = document.querySelector('#feed')
+
+fetch('http://localhost:8000/results')
+  .then(response => {return response.json()})
+  .then(data => {
+    data.forEach(article => {
+      const articlePost = `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`
+      newsFeed.insertAdjacentHTML("beforeend", articlePost)
+    })
+  })
+  .catch(err => console.log(err))
